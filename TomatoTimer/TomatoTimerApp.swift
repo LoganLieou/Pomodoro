d37fa18e34b64bed5f35e880258d07ff092d88e6
loganlieou <logan.lieou@gmail.com>
2025-03-28 17:26:25 -0500

    Initial Commit

---
+++ TomatoTimer/TomatoTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoTimerApp.swift
+//  TomatoTimer
+//
+//  Created by Logan  Jackson on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TomatoTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TomatoTimerTests/TomatoTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoTimerTests.swift
+//  TomatoTimerTests
+//
+//  Created by Logan  Jackson on 3/28/25.
+//
+
+import Testing
+@testable import TomatoTimer
+
+struct TomatoTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
